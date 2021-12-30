99118ed59b8e185b9e30c8aace4a56e76357e096
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-12-30 10:42:31 -0300

    Changes migrations so they are equivalent to the database project

---
+++ api/src/main/resources/db/migration/V1_1_1__insert_into_table_cliente.sql
@@ -0,0 +1,107 @@
+INSERT INTO cliente (nome_completo, dt_nascimento, sexo, estado_civil, cpf, email, celular, cep, logradouro, bairro,
+numero, complemento, estado, cidade) VALUES (
+    'Fernanda Brenda Lúcia Viana',
+    '1965-03-19',
+    'FEMININO',
+    'VIUVO',
+    '98939497180',
+    'fernanda_b@email.com',
+    '61983401268',
+    '70790104',
+    'Quadra SGAN 910 Módulo D',
+    'Asa Norte',
+    467,
+    '',
+    'DF',
+    'Brasília'
+);
+
+INSERT INTO cliente (nome_completo, dt_nascimento, sexo, estado_civil, cpf, email, celular, cep, logradouro, bairro,
+numero, complemento, estado, cidade) VALUES (
+    'Cláudia Rebeca Maitê Pires',
+    '1985-07-05',
+    'FEMININO',
+    'DIVORCIADO',
+    '99390669103',
+    'claudia_r@email.com',
+    '61992421425',
+    '70670104',
+    'Quadra SQSW 101 Bloco D',
+    'Setor Sudoeste',
+    786,
+    '',
+    'DF',
+    'Brasília'
+);
+
+INSERT INTO cliente (nome_completo, dt_nascimento, sexo, estado_civil, cpf, email, celular, cep, logradouro, bairro,
+numero, complemento, estado, cidade) VALUES (
+    'Leandro Vitor Pires',
+    '1981-06-03',
+    'MASCULINO',
+    'CASADO',
+    '59514856147',
+    'leandro_v@email.com',
+    '61987080695',
+    '70670205',
+    'Quadra SQSW 102 Bloco E',
+    'Setor Sudoeste',
+    790,
+    '',
+    'DF',
+    'Brasília'
+);
+
+INSERT INTO cliente (nome_completo, dt_nascimento, sexo, estado_civil, cpf, email, celular, cep, logradouro, bairro,
+numero, complemento, estado, cidade) VALUES (
+    'Thiago Igor Assunção',
+    '1979-11-24',
+    'MASCULINO',
+    'SOLTEIRO',
+    '54145830130',
+    'thiago_i@email.com',
+    '61989482817',
+    '73358220',
+    'Quadra 25 Conjunto D',
+    'Setor Residencial Leste (Planaltina)',
+    494,
+    '',
+    'DF',
+    'Brasília'
+);
+
+INSERT INTO cliente (nome_completo, dt_nascimento, sexo, estado_civil, cpf, email, celular, cep, logradouro, bairro,
+numero, complemento, estado, cidade) VALUES (
+    'Isabella Letícia Priscila da Rocha',
+    '1972-04-09',
+    'FEMININO',
+    'CASADO',
+    '38465400130',
+    'isabella_l@email.com',
+    '61995589238',
+    '72505103',
+    'Quadra AC 105 Bloco C',
+    'Santa Maria',
+    848,
+    '',
+    'DF',
+    'Brasília'
+);
+
+INSERT INTO cliente (nome_completo, dt_nascimento, sexo, estado_civil, cpf, email, celular, cep, logradouro, bairro,
+numero, complemento, estado, cidade) VALUES (
+    'Bruno Henry Francisco Pereira',
+    '2000-05-12',
+    'MASCULINO',
+    'SOLTEIRO',
+    '80170419185',
+    'bruno_h@email.com',
+    '61984457252',
+    '72610453',
+    'Avenida Eucalipto Quadra 204 Conjunto 3',
+    'Recanto das Emas',
+    808,
+    '',
+    'DF',
+    'Brasília'
+);
