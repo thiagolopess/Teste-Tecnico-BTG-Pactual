a05eb2bce99e7718264e292365a750dd83d5d64e
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-12-31 00:05:25 -0300

    Creates database migrations for table 'conta'

--- api/src/main/resources/db/migration/V1_2_0__create_table_conta.sql
+++ api/src/main/resources/db/migration/V1_2_0__create_table_conta.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS conta (
   num_conta BIGINT NOT NULL AUTO_INCREMENT,
   agencia INT NOT NULL,
-  saldo DECIMAL(12, 2) NOT NULL DEFAULT 0,
+  saldo FLOAT NOT NULL DEFAULT 0,
   dt_abertura DATE NOT NULL,
   dt_encerramento DATE,
   cpf VARCHAR(11) NOT NULL,
-  CONSTRAINT CONTA_PK PRIMARY KEY(num_conta),
-  CONSTRAINT CONTA_CLIENTE_FK FOREIGN KEY(cpf) REFERENCES cliente(cpf)
+  CONSTRAINT conta_pk PRIMARY KEY(num_conta),
+  CONSTRAINT conta_cliente_fk FOREIGN KEY(cpf) REFERENCES cliente(cpf)
 ) engine = innoDB AUTO_INCREMENT = 1;
