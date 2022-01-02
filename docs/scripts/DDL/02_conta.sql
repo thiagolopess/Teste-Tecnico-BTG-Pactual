c890a42c8e629097b21be781212bcedc33070f39
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2022-01-02 08:30:10 -0300

    Updates project's documentation

---
+++ docs/scripts/DDL/02_conta.sql
@@ -0,0 +1,10 @@
+-- Cria tabela `conta` (DDL)
+CREATE TABLE IF NOT EXISTS conta (
+  num_conta BIGINT NOT NULL AUTO_INCREMENT,
+  agencia INT NOT NULL,
+  saldo FLOAT NOT NULL DEFAULT 0,
+  dt_abertura DATE NOT NULL,
+  cpf VARCHAR(11) NOT NULL,
+  CONSTRAINT CONTA_PK PRIMARY KEY(num_conta),
+  CONSTRAINT CONTA_CLIENTE_FK FOREIGN KEY(cpf) REFERENCES cliente(cpf)
+) engine = innoDB AUTO_INCREMENT = 1;
