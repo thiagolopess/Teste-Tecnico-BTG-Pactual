e77c903512d695cdf4389cc2a5b13a0cd5d7c5d3
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2022-01-01 22:31:31 -0300

    Removes 'dt_encerramento' from migration

--- api/src/main/resources/db/migration/V1_2_0__create_table_conta.sql
+++ api/src/main/resources/db/migration/V1_2_0__create_table_conta.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS conta (
   agencia INT NOT NULL,
   saldo FLOAT NOT NULL DEFAULT 0,
   dt_abertura DATE NOT NULL,
-  dt_encerramento DATE,
   cpf VARCHAR(11) NOT NULL,
   CONSTRAINT conta_pk PRIMARY KEY(num_conta),
   CONSTRAINT conta_cliente_fk FOREIGN KEY(cpf) REFERENCES cliente(cpf)
